070b3f4d3707b92edddde3d3a527a9ed2e98fe10
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-11-04 14:38:24 -0300

    Inicializando backend

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+
+
+## server: localhost
+## user: id15255706_root
+## senha: >f?WWz3YQwIVJI(9
+## banco: id15255706_itrampo
---
+++ backend/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="controllers/login.php" method="post">
+            <input type="email" name="usuario" placeholder="User name" id=""><br>
+            <input type="password" name="senha" placeholder="senha"><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
